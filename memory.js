2c28cc7546ae0db5a0f5e6c51213a3d6e85e3bb6
jkojro <janusz.kojro@gmail.com>
2018-04-19 20:54:18 +0200

    pojawianie się kolejnych rzędów

--- memory.js
+++ memory.js
@@ -14,15 +14,56 @@ $('#c4').on('click', function() { chooseColor(0, 4);});
 $('#c5').on('click', function() { chooseColor(0, 5);});
 
 $('#go1').on('click', function (){ go(1); });
+$('#go2').on('click', function (){ go(2); });
+$('#go3').on('click', function (){ go(3); });
+$('#go4').on('click', function (){ go(4); });
+$('#go5').on('click', function (){ go(5); });
+$('#go6').on('click', function (){ go(6); });
 
 $('#e0').on('click', function() {chooseColor(1, 0); });
 $('#e1').on('click', function() {chooseColor(1, 1); });
 $('#e2').on('click', function() {chooseColor(1, 2); });
 $('#e3').on('click', function() {chooseColor(1, 3); });
 
+$('#e10').on('click', function() {chooseColor(2, 10); });
+$('#e11').on('click', function() {chooseColor(2, 11); });
+$('#e12').on('click', function() {chooseColor(2, 12); });
+$('#e13').on('click', function() {chooseColor(2, 13); });
+
+$('#e20').on('click', function() {chooseColor(3, 20); });
+$('#e21').on('click', function() {chooseColor(3, 21); });
+$('#e22').on('click', function() {chooseColor(3, 22); });
+$('#e23').on('click', function() {chooseColor(3, 23); });
+
+$('#e30').on('click', function() {chooseColor(4, 30); });
+$('#e31').on('click', function() {chooseColor(4, 31); });
+$('#e32').on('click', function() {chooseColor(4, 32); });
+$('#e33').on('click', function() {chooseColor(4, 33); });
+
+$('#e40').on('click', function() {chooseColor(5, 40); });
+$('#e41').on('click', function() {chooseColor(5, 41); });
+$('#e42').on('click', function() {chooseColor(5, 42); });
+$('#e43').on('click', function() {chooseColor(5, 43); });
+
+$('#e50').on('click', function() {chooseColor(6, 50); });
+$('#e51').on('click', function() {chooseColor(6, 51); });
+$('#e52').on('click', function() {chooseColor(6, 52); });
+$('#e53').on('click', function() {chooseColor(6, 53); });
+
 
 //$('#c0').on("click", () => {  $('#c0').chooseColor(0);}
 //$(#c2).on("click", () => { function() chooseColor(2);}
+
+function go(nr) {
+	$('#ch'+nr).addClass('done_choice');
+	if (nr-1 > 0) 
+	{
+		$('#ch'+(nr-1)).addClass('actual_choice');
+		$('#ch'+(nr-1)).removeClass('choice');
+	}
+	
+}
+
 
 var colorActive = false;
 var holeActive = false;
@@ -40,7 +81,7 @@ function chooseColor(row, nr) {
 		selectedColor = color_palette[nr];
 		
 		//alert( selectedColor);
-	} else if(row == 1)
+	} else
 	{
 		holeActive = true;
 		$('#e'+nr).toggleClass('emptyActive');
@@ -50,7 +91,6 @@ function chooseColor(row, nr) {
 			image = "url(img/"+selectedColor+")";
 			$('#e'+nr).css('background-image', image);
 			$('#e'+nr).removeClass('emptyActive');
-			//alert( image);
 		}
 	}
 }
